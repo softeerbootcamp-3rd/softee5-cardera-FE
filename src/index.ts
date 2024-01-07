54d1aa3314345bf19420581d6a9aa4730206bed6
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-07 17:25:58 +0900

    feat: 출발지/도착지, 편도/왕복 정보 - 다음 우편번호 검색 API 이용

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,29 @@ type Step = (typeof steps)[number]
 const stepElements = document.querySelectorAll(
   '[data-step]'
 ) as NodeListOf<HTMLSelectElement>
+const addressForm = document.getElementById('address') as HTMLUListElement
+const carpoolTypeSelect = document.getElementById(
+  'carpool-type-select'
+) as HTMLUListElement
+const startRoadFullAddressInput = document.getElementById(
+  'startRoadFullAddr'
+) as HTMLInputElement
+const destRoadFullAddressInput = document.getElementById(
+  'destRoadFullAddr'
+) as HTMLInputElement
 
+function getCurrentStep(): Step {
+  return Array.from(stepElements).find(
+    (element) => !element.classList.contains('hidden')
+  )?.dataset.step as Step
+}
+
+function getCurrentStepIndex() {
+  return steps.indexOf(getCurrentStep())
+}
 /**
  * step에 따라 화면에 스텝을 노출
+ * TODO session storage 에서 data resotre 해오기
  * @param step 화면에 스텝
  */
 function showStep(step: Step) {
@@ -29,38 +49,79 @@ function showStep(step: Step) {
   })
 }
 
-/**
- * 뒤로가기, 앞으로가기 버튼 클릭 시 step 전환하는 클릭 이벤트 등록
- */
-function clickPrevAndNextButton() {
-  stepElements.forEach((stepElement) => {
-    const currentStep = stepElement.dataset.step as Step
-    const currentStepIndex = steps.findIndex((step) => step === currentStep)
+function registerJusoStep() {
+  // 출발지, 도착지 주소 정보 검색
+  addressForm.addEventListener('click', ({ target }) => {
+    if (!target) return
+    if (target instanceof HTMLInputElement) {
+      // @ts-ignore
+      new daum.Postcode({
+        oncomplete: (data) => {
+          let address = data.address
+          if (target.id === 'startRoadFullAddr') {
+            target.value = address
+          }
+          if (target.id === 'destRoadFullAddr') {
+            target.value = address
+          }
+        },
+      }).open()
+    }
+  })
+
+  // 편도, 왕복 선택
+  carpoolTypeSelect.addEventListener('click', ({ target }) => {
+    if (!target) return
+    if (target instanceof Element) {
+      // TODO: style 바꾸기, data-value 심기
+      const carpoolType = (target as HTMLButtonElement).dataset.carpoolType
+      carpoolTypeSelect.dataset.value = carpoolType
+    }
+  })
+
+  // 스텝 변경
+  const prevButton = document.querySelector(
+    '#juso-step-button > .prev-button'
+  ) as HTMLElement
+  const nextButton = document.querySelector(
+    '#juso-step-button > .next-button'
+  ) as HTMLElement
+  prevButton.addEventListener('click', () => {
+    // TODO prev step 구하는 로직 분리
+    const currentStep = getCurrentStep()
+    const currentStepIndex = getCurrentStepIndex()
     const prevStep =
       currentStepIndex === 0 ? currentStep : steps[currentStepIndex - 1]
+    showStep(prevStep)
+  })
+  nextButton.addEventListener('click', () => {
+    // TODO next step 구하는 로직 분리
+    const currentStep = getCurrentStep()
+    const currentStepIndex = getCurrentStepIndex()
     const nextStep =
       currentStepIndex === steps.length - 1
         ? currentStep
         : steps[currentStepIndex + 1]
-
-    stepElement.addEventListener('click', ({ target }) => {
-      if (!target) return
-
-      if (target instanceof Element) {
-        if (target.id === 'prev-step') {
-          showStep(prevStep)
-        }
-        if (target.id === 'next-step') {
-          showStep(nextStep)
-        }
-      }
-    })
+    showStep(nextStep)
   })
 }
 
+function registerPassengerCountStep() {}
+
+function registerCarpoolCountStep() {}
+
+function registerResultStep() {}
+
+function fetchFuelPrice() {
+  // TODO: result step이 active 할 때 서버에서 유류비 값 가져오기
+}
+
 function main() {
-  showStep('intro')
-  clickPrevAndNextButton()
+  showStep('juso')
+  registerJusoStep()
+  registerPassengerCountStep()
+  registerCarpoolCountStep()
+  registerResultStep()
 }
 
 main()
