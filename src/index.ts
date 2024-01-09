ab0eb8ce8c8ae00ea80342298cdd1fc49ca63090
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-10 02:43:47 +0900

    fix: 카카오 우편번호 찾기 서비스 iframe으로 삽입하도록 수정 - 기존의 popup방식은 pwa에서 뒤로 갈 수 없는 이슈가 있었음

--- src/index.ts
+++ src/index.ts
@@ -197,9 +197,15 @@ function registerIntroStep() {
 }
 
 function registerJusoStep() {
+  const adressServiceWrapper = document.getElementById('address-service-wrap') as HTMLDivElement
+  const addressServiceCloseBtn = document.querySelector('#address-service-wrap .close') as HTMLButtonElement
   // 출발지, 도착지 주소 정보 검색
   addressForm.addEventListener('click', ({ target }) => {
     if (!target) return
+
+    // iframe을 넣은 element를 보이게 한다.
+    adressServiceWrapper.style.display = 'block'
+    const currentScroll = Math.max(document.body.scrollTop, document.documentElement.scrollTop)
     if (target instanceof HTMLInputElement) {
       // @ts-ignore
       new daum.Postcode({
@@ -211,9 +217,26 @@ function registerJusoStep() {
           if (target.id === 'destRoadFullAddr') {
             target.value = address
           }
+          // iframe을 넣은 element를 안보이게 한다.
+          // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
+          adressServiceWrapper.style.display = 'none'
+
+          // 우편번호 찾기 화면이 보이기 이전으로 scroll 위치를 되돌린다.
+          document.body.scrollTop = currentScroll
         },
-      }).open()
+        // 우편번호 찾기 화면 크기가 조정되었을때 실행할 코드를 작성하는 부분. iframe을 넣은 element의 높이값을 조정한다.
+        onresize: function (size) {
+          adressServiceWrapper.style.height = size.height + 'px'
+        },
+        width: '100%',
+        height: '200%',
+      }).embed(adressServiceWrapper)
     }
+  })
+
+  // 주소검색 서비스 닫기
+  addressServiceCloseBtn.addEventListener('click', () => {
+    adressServiceWrapper.style.display = 'none'
   })
 
   // 스텝 변경
